879ad4457bf3a610b46c57b67f4b77b7981f0bc5
¨mago¨ <martingramajo08@gmail.com>
2023-01-08 17:42:27 -0300

    limpio componente de elementos no utilizados

--- src/pages/Resp.jsx
+++ src/pages/Resp.jsx
@@ -1,20 +1,8 @@
-import React, { useState } from "react";
-import { Spinner } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import React from "react";
 import Back from "../components/Back";
 import RespuestasContenido from "../components/RespuestasContenido";
 
 export default function Resp({ datos }) {
-  const [loading, setLoading] = useState(false);
-
-  const cargando = () => {
-    setLoading(true);
-  };
-
-  const completado = () => {
-    setLoading(false);
-  };
-
   return (
     <div className="container p-4 ">
       <h1 className="fs-4 text-center">Formulario Firebase Data</h1>
