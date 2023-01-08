1875db4001a871cda0b5623851018431cb7556eb
¨mago¨ <martingramajo08@gmail.com>
2023-01-08 16:31:11 -0300

    agrego spinner de carga al enviar el formulario a firebase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Resp from './pages/Resp';
@@ -6,17 +7,19 @@ import { db } from "./config/firebase"
 
 
 function App() {
-
+  const [loading, setLoading] = useState(false);
 
   const addFirebase = async (dataObject) => {
+    setLoading(true);
     await db.collection('Formulario').doc().set(dataObject)
+    setLoading(false);
     console.log('nuevo campo agregado en firebase')
   };
 
   return (
     <BrowserRouter>
       <Routes >
-        <Route path="/" element={<Home addFirebase={addFirebase} />} />
+        <Route path="/" element={<Home addFirebase={addFirebase} loading={loading} />} />
         <Route path="/respuestas" element={<Resp />} />
       </Routes>
     </BrowserRouter>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import Item from "../components/Item";
 import datos from "../db/db.json";
+import { Spinner } from "react-bootstrap";
 
-export default function Home({ addFirebase }) {
-  const items = datos.items;
+export default function Home({ addFirebase, loading }) {
   // state initial de los inputs
   const initialStateValues = {
     full_name: "",
@@ -13,13 +13,13 @@ export default function Home({ addFirebase }) {
     terms_and_conditions: "",
   };
 
+  const items = datos.items;
   const [values, setValues] = useState(initialStateValues);
-  console.log("file: Home.jsx:17 ~ Home ~ values", values);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     addFirebase(values);
-    setValues(...initialStateValues);
+    setValues({ ...initialStateValues });
   };
 
   const handleInputChange = (e) => {
@@ -30,11 +30,22 @@ export default function Home({ addFirebase }) {
   return (
     <div className="container p-4">
       <h1 className="text-center pb-2"> Challenge Greydive</h1>
-      <form className="card card-body" onSubmit={handleSubmit}>
-        {items.map((item) => (
-          <Item dato={item} handleInputChange={handleInputChange} />
-        ))}
-      </form>
+      {loading ? (
+        <div className="d-flex justify-content-center align-content-center align-items-center p-5 my-5">
+          <Spinner
+            className="fs-6"
+            animation="border"
+            role="status"
+            variant="light"
+          ></Spinner>
+        </div>
+      ) : (
+        <form className="card card-body" onSubmit={handleSubmit}>
+          {items.map((item) => (
+            <Item dato={item} handleInputChange={handleInputChange} />
+          ))}
+        </form>
+      )}
     </div>
   );
 }
