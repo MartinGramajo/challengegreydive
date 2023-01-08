64db302d53a4d2e2441ff53d7e02bda733616b5e
¨mago¨ <martingramajo08@gmail.com>
2023-01-07 22:21:36 -0300

    agrego la func handlesubmit y handleinputchange

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "../components/Item";
-import datos from "../db.json";
+import datos from "../db/db.json";
 
 export default function Home() {
   const items = datos.items;
+  // state initial de los inputs
+  const initialStateValues = {
+    full_name: "",
+    email: "",
+    birth_date: "",
+    country_of_origin: "Argentina",
+    terms_and_conditions: "",
+  };
+
+  const [values, setValues] = useState(initialStateValues);
+  console.log("file: Home.jsx:17 ~ Home ~ values", values);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
 
   return (
-    <div className="container p-5">
-      <h1 className="text-center pb-5"> Challenge Greydive</h1>
-      <form className="card card-body">
+    <div className="container p-4">
+      <h1 className="text-center pb-2"> Challenge Greydive</h1>
+      <form className="card card-body" onSubmit={handleSubmit}>
         {items.map((item) => (
-          <Item dato={item} />
+          <Item dato={item} handleInputChange={handleInputChange} />
         ))}
       </form>
     </div>
